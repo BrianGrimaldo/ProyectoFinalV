5afb97cb2d6079954da21c006056e5daeb79fad4
BrianGrimaldo <bgrimaldo65@gmail.com>
2024-06-17 08:36:35 -0600

    Se agregaron los test

--- src/main/java/com/example/AgenciaAutos/Cliente.java
+++ src/main/java/com/example/AgenciaAutos/Cliente.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.Data;
 
-@Document(collection = "clients")
+@Document(collection = "clientes")
 @Data
 public class Cliente {
     @Id
